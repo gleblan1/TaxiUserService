86f67d29a102a339d79dd5d5ccc83848a53627a9
gleblan1 <kekskiylol@gmail.com>
2024-05-06 10:23:18 +0200

    fix(userservice): added new enum type for taxi types[86byangcd]

--- migrations/trips/20240502071306_trips.sql
+++ migrations/trips/20240502071306_trips.sql
@@ -1,25 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
+CREATE TYPE taxi_type AS ENUM('economy', 'comfort', 'business');
 CREATE TABLE IF NOT EXISTS trips(
     id BIGSERIAL PRIMARY KEY,
     from_address VARCHAR(255) NOT NULL,
     to_address VARCHAR(255) NOT NULL,
     rate INT NOT NULL DEFAULT 0,
-    taxi_type INT,
+    taxi taxi_type,
     user_id INT NOT NULL,
     CONSTRAINT fk_user_trips FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-CREATE TABLE IF NOT EXISTS taxi_types(
-     id SERIAL PRIMARY KEY,
-     type ENUM('economy', 'comfort', 'business')
-);
-
-INSERT INTO taxi_types (type) VALUES ('economy'), ('comfort'), ('business');
-
-ALTER TABLE taxi_types ALTER COLUMN type SET DEFAULT 'economy';
-
-ALTER TABLE IF EXISTS trips ADD CONSTRAINT fk_taxi_type_trips FOREIGN KEY (taxi_type) REFERENCES taxi_types(id);
 -- +goose StatementEnd
 
 -- +goose Down
--- repositories/trips.go
+++ repositories/trips.go
@@ -17,7 +17,7 @@ type TripModel struct {
 
 func (r *Repository) GetLastTrip(ctx context.Context, user models.User) (models.Trip, error) {
 	trip := TripModel{}
-	err := r.db.QueryRow("SELECT id, taxi_type, from_address, to_address, rate FROM trips WHERE user_id=$1 ORDER BY id DESC LIMIT 1", 32).Scan(&trip.id, &trip.taxiType, &trip.from, &trip.to, &trip.rate)
+	err := r.db.QueryRow("SELECT id, taxi, from_address, to_address, rate FROM trips WHERE user_id=$1 ORDER BY id DESC LIMIT 1", 32).Scan(&trip.id, &trip.taxiType, &trip.from, &trip.to, &trip.rate)
 	if err != nil {
 		fmt.Println(trip)
 		return models.Trip{}, err
@@ -45,7 +45,7 @@ func (r *Repository) RateTrip(ctx context.Context, tripId int, rate int) (float3
 
 func (r *Repository) GetTripsHistory(ctx context.Context, user models.User) ([]models.Trip, error) {
 	var result []models.Trip
-	rows, err := r.db.Query("SELECT id, taxi_type, from_address, to_address, rate FROM trips WHERE user_id=$1 ORDER BY id DESC LIMIT 1", 32)
+	rows, err := r.db.Query("SELECT id, taxi, from_address, to_address, rate FROM trips WHERE user_id=$1 ORDER BY id DESC LIMIT 1", 32)
 	if err != nil {
 		return result, err
 	}
